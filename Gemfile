ec058a4a7b170e3cc5574a91622cbb266abfffb4
Jasper King-Harman <j.kingharman@googlemail.com>
2018-04-08 16:36:05 +0100

    Add weather service class

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "dovent"
 gem "httparty"
 gem "papirus"
 gem "rmagick"
+gem "dotenv"
--- lib/ldn_weather_service.rb
+++ lib/ldn_weather_service.rb
@@ -1,32 +1,35 @@
 require "httparty" # for now (will be extracted)
+require_relative "log.rb"
 
 class LondonWeatherService
-  OPEN_WEATHER_KEY = '011d3bba13efcf724c886b7185e2d0fa'.freeze
-  LONDON_ID = '2643743'.freeze
+  OPEN_WEATHER_KEY = ENV['OPEN_WEATHER'].freeze
+  LONDON_ID = ENV['LONDON_ID'].freeze
   BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?id=#{LONDON_ID}&APPID=#{OPEN_WEATHER_KEY}".freeze
 
-  def initialize(client: client_obj)
+  def initialize(client = HTTParty, log: Log)
     @client = client
+    @log = log
+  end
+
+  def call
+    response = request_forecast
+    get_icon_code(response)
   end
 
   private
 
   def request_forecast
-    begin
-      response = HTTParty.get(BASE_URL)
-    rescue
-      # here, you'll need to be responsible and write to a logfile.
-      # can you find out what the error response looks like from docs?
-      # you'll want to return some indication of error that will be passed
-      # around. break if error.
-    end
+    HTTParty.get(BASE_URL)
+  rescue => err
+    Log.instance.info err
+    :error
   end
 
   def get_icon_code(response)
-    begin
-      response['list'].first['weather'].first['icon']
-    rescue
-      # control what is returned, but log or see error message to know what went wrong.
+    response['list'].first['weather'].first['icon']
+  rescue => err
+    Log.instance.info err
+    :error
   end
 
   attr_reader :client
